f05264879e8674d34d697ab2f7d368f2f12f193a
Yaqi <yaqiye22@gmail.com_>
2023-03-28 17:38:11 +0200

    Sección 21 terminada. 28_03_2023

---
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+      #mapa {
+        height: 100%;
+        width: 100%
+      }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -7.050551, 43.464597 ],
+      zoom: 18
+    });
+    
+  }
+}
